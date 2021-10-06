4f8e794e06fd50716b55dbf6efc2d4784b34f6e7
Alexander Travkin <alexander.travkin@varwin.com>
2021-10-06 18:14:15 +0300

    Add Signals with 3 and 4 generic-parameters

---
+++ Signal3.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+namespace AffenSignals
+{
+    public abstract class Signal<T1, T2, T3> : ISignal
+    {
+        private readonly HashSet<Action> _listenersWithoutParameters = new HashSet<Action>();
+        private readonly HashSet<Action<T1, T2, T3>> _listeners = new HashSet<Action<T1, T2, T3>>();
+
+        public void AddListener(Action listener)
+        {
+            _listenersWithoutParameters.Add(listener);
+        }
+
+        public void AddListener(Action<T1, T2, T3> listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void RemoveListener(Action listener)
+        {
+            _listenersWithoutParameters.Remove(listener);
+        }
+
+        public void RemoveListener(Action<T1, T2, T3> listener)
+        {
+            _listeners.Remove(listener);
+        }
+
+        public void RemoveAllListeners()
+        {
+            _listeners.Clear();
+            _listenersWithoutParameters.Clear();
+        }
+
+        public void Invoke(T1 arg0, T2 arg1, T3 arg2)
+        {
+            var listeners = new HashSet<Action<T1, T2, T3>>(_listeners);
+            foreach (var listener in listeners)
+            {
+                listener?.Invoke(arg0, arg1, arg2);
+            }
+
+            listeners.Clear();
+
+            var listenersWithoutParameters = new HashSet<Action>(_listenersWithoutParameters);
+            foreach (var listener in listenersWithoutParameters)
+            {
+                listener?.Invoke();
+            }
+
+            listenersWithoutParameters.Clear();
+        }
+    }
+}
