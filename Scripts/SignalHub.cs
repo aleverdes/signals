9a220e5f54ba0f7f3f7f6b87cbb3f9a5556d55f7
Alexander Travkin <alexander.travkin@varwin.com>
2022-03-25 12:13:03 +0300

    Added AgressiveInlining

--- Scripts/SignalHub.cs
+++ Scripts/SignalHub.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace AffenSignals
 {
@@ -8,25 +9,31 @@ namespace AffenSignals
         private static readonly HashSet<Action> RegisteredListeners = new();
         private static readonly HashSet<Action<T>> RegisteredListenersWithArg = new();
 
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public void AddListener(Action listener)
         {
             RegisteredListeners.Add(listener);
         }
+        
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public void AddListener(Action<T> listener)
         {
             RegisteredListenersWithArg.Add(listener);
         }
 
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public void RemoveListener(Action listener)
         {
             RegisteredListeners.Remove(listener);
         }
 
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public void RemoveListener(Action<T> listener)
         {
             RegisteredListenersWithArg.Remove(listener);
         }
 
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public void Invoke(T arg)
         {
             foreach (var registeredListener in RegisteredListeners)
--- Scripts/Signals.cs
+++ Scripts/Signals.cs
@@ -1,32 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace AffenSignals
 {
     public static class Signals<T> where T : struct, ISignal
     {
         private static readonly SignalHub<T> DefaultHub = new();
-
+        
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public static void AddListener(Action listener)
         {
             DefaultHub.AddListener(listener);
         }
         
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public static void AddListener(Action<T> listener)
         {
             DefaultHub.AddListener(listener);
         }
 
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public static void RemoveListener(Action listener)
         {
             DefaultHub.RemoveListener(listener);
         }
 
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public static void RemoveListener(Action<T> listener)
         {
             DefaultHub.RemoveListener(listener);
         }
 
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public static void Invoke(T arg)
         {
             DefaultHub.Invoke(arg);
